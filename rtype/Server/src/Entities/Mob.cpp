43e1cc06af9d20121a511574f3cd04d33acc09a2
Alexis Mestag <mestag_a@epitech.eu>
2013-11-18 16:08:35 +0100

    tout plein de trucs

--- rtype/Server/src/Entities/Mob.cpp
+++ rtype/Server/src/Entities/Mob.cpp
@@ -5,7 +5,7 @@
 // Login   <maitre_c@epitech.net>
 // 
 // Started on  Wed Oct 30 11:19:56 2013 antoine maitre
-// Last update Mon Nov 18 13:24:08 2013 alexis mestag
+// Last update Mon Nov 18 15:11:25 2013 alexis mestag
 //
 
 #include "Entities/Mob.hh"
@@ -18,11 +18,14 @@ Mob::Mob(int const x, int const y, std::string const &path, int const speed)
 
   std::getline(fichier, contenu);
   contenu = contenu.substr(contenu.find("=") + 1, contenu.size() - contenu.find("="));
-  this->_destructible = static_cast<bool>(std::stoi(contenu));
+  if (contenu != "")
+    this->_destructible = static_cast<bool>(std::stoi(contenu));
   std::getline(fichier, contenu);
   contenu = contenu.substr(contenu.find("=") + 1, contenu.size() - contenu.find("="));
-  this->_width = std::stoi(contenu);
-  this->_height = std::stoi(contenu.substr(contenu.find("x") + 1, contenu.size() - contenu.find("x")));
+  if (contenu != "") {
+    this->_width = std::stoi(contenu);
+    this->_height = std::stoi(contenu.substr(contenu.find("x") + 1, contenu.size() - contenu.find("x")));
+  }
   this->_weapon.setWeapon(AProjectile::ROCKET);
 }
 
@@ -34,4 +37,10 @@ Mob::~Mob()
 AEntity::eObject		Mob::getType() const
 {
   return (AEntity::MOB);
+}
+
+Action const			*Mob::getAction() const
+{
+  _action->reset();
+  return (_action);
 }
