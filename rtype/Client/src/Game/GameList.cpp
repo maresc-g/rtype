1c6a0d20161c8ca239bffb48046715ef078b74f9
Guillaume Marescaux <maresc_g@epitech.eu>
2013-11-20 13:11:14 +0100

    fuck cyril

--- rtype/Client/src/Game/GameList.cpp
+++ rtype/Client/src/Game/GameList.cpp
@@ -5,7 +5,7 @@
 // Login   <maresc_g@epitech.net>
 // 
 // Started on  Tue Oct 29 15:03:05 2013 guillaume marescaux
-// Last update Wed Nov 13 16:55:48 2013 cyril jourdain
+// Last update Wed Nov 20 13:10:52 2013 guillaume marescaux
 //
 
 #include			"Game/GameList.hh"
@@ -16,31 +16,6 @@ GameList::GameList():
   _games(new std::list<GameInfo *>), _mutex(new Mutex)
 {
   _mutex->initialize();
-
-  GameInfo *t = new GameInfo("1", "name1");
-  GameInfo *t2 = new GameInfo("2", "name2");
-  GameInfo *t3 = new GameInfo("3", "name3");
-  GameInfo *t4 = new GameInfo("4", "name4");
-  GameInfo *t5 = new GameInfo("5", "name5");
-  GameInfo *t6 = new GameInfo("6", "name6");
-  GameInfo *t7 = new GameInfo("7", "name7");
-  GameInfo *t8 = new GameInfo("8", "name8");
-  GameInfo *t9 = new GameInfo("9", "name9");
-  GameInfo *t10 = new GameInfo("10", "name10");
-  GameInfo *t11= new GameInfo("11", "name11");
-  GameInfo *t12 = new GameInfo("12", "name12");
-  _games->push_back(t);
-  _games->push_back(t2);
-  _games->push_back(t3);
-  _games->push_back(t4);
-  _games->push_back(t5);
-  _games->push_back(t6);
-  _games->push_back(t7);
-  _games->push_back(t8);
-  _games->push_back(t9);
-  _games->push_back(t10);
-  _games->push_back(t11);
-  _games->push_back(t12);
 }
 
 GameList::~GameList()
