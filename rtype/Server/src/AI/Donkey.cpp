7a61543d8cf801f746ea1287db838abcb7d9c7c4
Alexis Mestag <mestag_a@epitech.eu>
2013-11-23 17:12:47 +0100

    Modif' Donkey

--- rtype/Server/src/AI/Donkey.cpp
+++ rtype/Server/src/AI/Donkey.cpp
@@ -5,7 +5,7 @@
 // Login   <mestag_a@epitech.net>
 // 
 // Started on  Mon Nov 18 10:34:21 2013 alexis mestag
-// Last update Sat Nov 23 11:20:17 2013 laurent ansel
+// Last update Sat Nov 23 16:20:42 2013 alexis mestag
 //
 
 #include			<time.h>
@@ -23,28 +23,14 @@ Donkey::~Donkey()
 
 Action const			*Donkey::getAction() const
 {
-  // static bool			start = true;
-  // static double			duration = 0;
-  // time_t			timer;
-  // time_t			timerEnd;
+  static int			willFire = 0;
 
-  // if (start)
-  //   {
-  //     time(&timer);
-  //     duration = 0;
-  //     start = false;
-  //   }
-  // time(&timerEnd);
+
   _action->reset();
   _action->setLeft(true);
-  // std::cout << "TIME = " << (double)timer << std::endl;
-  // std::cout << "TIME = " << (double)timerEnd << std::endl;
-  // duration += difftime(timerEnd, timer);
-  // std::cout << "DURATION = " << duration << std::endl;
-  // if (duration > 500000000)
-  //   {
-  //     //      _action->setFire(true);
-  //     start = true;
-  //   }
+  if (willFire++ == 100) {
+    _action->setFire(true);
+    willFire = 0;
+  }
   return (_action);
 }
